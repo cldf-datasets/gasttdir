1b10fcb1045f463798af459cc858a8126a6d9714
Johannes Englisch <johannes_englisch@eva.mpg.de>
2023-01-24 12:02:31 +0100

    created a cldfbench

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_gasttdir',
+    py_modules=['cldfbench_gasttdir'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'gasttdir=cldfbench_gasttdir:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
