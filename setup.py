db7ed418c7a476a2c8f0951b16e4026d1e50ab9c
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:06:15 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_gasttdir',
-    py_modules=['cldfbench_gasttdir'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'gasttdir=cldfbench_gasttdir:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog,excel]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
